7e73837ecd3de4449718eb30353b3d9949966fd6
japostad <japostad@student.42barcelona.com>
2025-03-16 14:37:31 +0100

    Add: replicate strdup

---
+++ ft_strdup.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ft_len(char *s)
+{
+    int i = 0;
+    if(!s)
+        return 0;
+    while (s[i] != '\0')
+        i++;
+    return i;
+}
+char *ft_strdup(char *src)
+{
+    char *s;
+    int len = ft_len(s);
+    int i = 0;
+    s = (char *)malloc(sizeof(char) * len + 1);
+    if (!s)
+        return NULL;
+    while (src[i])
+    {
+        s[i] = src[i];
+        i++;
+    }
+    s[i] = '\0';
+    return s;
+}
+int main()
+{
+    char *s;
+    s = ft_strdup("test");
+    printf("test\n");
+    printf("%s\n",s);
+    return 0;
+}
+
