cbd7ae329b9a0e3654af9468bd95e4b265c83aae
japostad <japostad@student.42barcelona.com>
2025-05-14 14:56:51 +0200

    Mod: shorter solution

--- ft_atoi_base.c
+++ ft_atoi_base.c
@@ -20,57 +20,50 @@
 /*                                                                                */
 /*int     ft_atoi_base(const char *str, int str_base);                            */
 
-int ft_isspace(char c)
+int sspace(char c)
 {
-    if (c <= 32)
-        return 1;
-    return 0;
+	return(c == ' ' || (c >= 9 && c<= 13));
+}
+int n_base(char s, int n)
+{
+	if(n <= 10)
+		return (s >= '0' && s<= '9');
+	return((s >= '0' && s<= '9') || (s >= 'a' && s <= ('a' + n - 10)) || (s >= 'A' && s<= ('A' + n - 10)));
+}
+int ft_atoi_base(const char *s, int n)
+{
+	int res=0;
+	int sign = 1;
+	if(!s || (n < 2 || n > 16))
+			return 0;
+	while(*s && sspace(*s))
+		s++;
+	if(*s == '-' || *s == '+')
+	{
+		if(*s == '-')
+			sign= -1;
+		s++;
+	}
+	while(*s && n_base(*s,n))
+	{
+		char c = *s;
+		if(c >= 'a' && c <= 'f')
+			res = res * n + (c - 'a' + 10);
+		else if(c >= 'A' && c <= 'F')
+			res = res * n + (c - 'A' + 10);
+		else
+			res = res * n + (c  - '0');
+		s++;
+	}
+	return (res * sign);
 }
 
-int isvalid(char s, int base)
-{
-    char b1[17] = "0123456789abcdef";
-    char b2[17] = "0123456789ABCDEF";
-    while (base--)
-        if (b1[base] == s || b2[base] == s)
-            return 1;
-    return 0;
-}
-int valueof(char c)
-{
-    if (c >= '0' && c <= '9')
-        return (c - '0');
-    else if (c >= 'a' && c <= 'f')
-        return (c - 'a' + 10);
-    else if (c >= 'A' && c <= 'F')
-        return (c - 'A' + 10);
-    else
-        return 0;
-}
-
-int ft_atoi_base(const char *s, int base)
-{
-    int res = 0;
-    int sign = 1;
-    while(ft_isspace(*s))
-          s++;
-    if(*s == '-' || *s == '+')
-    {
-        if (*s == '-')
-            sign = -1;
-        s++;
-    }
-    while(isvalid(*s, base))
-        res = res * base + valueof(*s++);
-    return (res * sign);
-}
 #include <stdio.h>
-/*
 int main()
 {
-	char *str = "5f";
+	char *str = "1A";
+	//1A == 26 in base 16;
 
-	printf("%d", ft_atoi_base(str, 10));
+	printf("%d", ft_atoi_base(str, 16));
 	return (0);
 }
-*/
