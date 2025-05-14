382e45441b09e20c624563317269133c0811b1c8
japostad <japostad@student.42barcelona.com>
2025-05-14 15:50:09 +0200

    Add: int to ascii

---
+++ itoa.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+char *ft_itoa(int nb)
+{
+	char *res;
+	long n; 
+	int len;
+	if(nb == INT_MIN)
+		return("-2147483648");
+	n =nb;
+	len = (n <= 0) ? 1:0;
+	while(n != 0)
+	{
+		n /= 10;
+		len++;
+	}
+	if(nb < 0)
+	{
+		nb = -nb;
+		len++;
+	}
+	res =(char *) malloc((len + 1) * sizeof(char));
+	if(!res)
+		return NULL;
+	res[len] = '\0';
+	if(nb == 0)
+		res[0] = '0';
+	while(nb!=0)
+	{
+		res[--len] = (nb % 10) + '0';
+		nb/=10;
+	}
+	return res;
+}
+
+int main()
+{
+	printf("%s\n", ft_itoa(INT_MAX));
+	printf("%s\n", ft_itoa(INT_MIN));
+}
