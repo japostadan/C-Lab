e6dddecae11e278a39230ae39472765f2646833d
japostad <japostad@student.42barcelona.com>
2025-05-14 11:56:55 +0200

    Mod: use pointers

--- rev_wstr.c
+++ rev_wstr.c
@@ -30,66 +30,40 @@
 
 #include <unistd.h>
 
-int		str_length(char *str)
-{
-	int	len = 0;
 
-	while (*str)
-	{
-		str++;
-		len++;
-	}
-	return (len);
+int		sspace(char c)
+{
+	return (c == ' ' || (c >= 9 && c <= 13));
 }
 
-int		iswhitespace(char chr)
+void	print_reverse(char *s)
 {
-	return (chr == ' ' || chr == '\t');
-}
-
-void	print_reverse(char *str)
-{
-	// let's use three pointers to traverse the string backwards
-	// the fast pointer will, dah, move faster then the others
-	// mainly:
-	int fast;	// will be placed at the start of a word
-	int slow;	// will be placed at the end of a word
-	int curr;	// will traverse the current word so we can write to stdout
-
-	// shall we start at the end of the string
-	fast = str_length(str) - 1;
-	slow = fast;
-	curr = fast;
-
-	// until we reach the beginning of the string
-	while (fast >= 0)
+	char *end = s;
+	while(*end)
+		end++;
+	end--;
+	while(end >= s && sspace(*end))
+		end--;
+	while(end >= s)
 	{
-		// here, fast should be at the last char of a word
-		// so we place every pointer in the same place
-		slow = fast;
-		curr = fast;
-		// now we place fast at the first char of the current word
-		while (fast >= 0 && !iswhitespace(str[fast]))
-			fast--;
-		fast++;
-		// we'll place curr where fast is, so we can write the curr word
-		curr = fast;
-		while (curr <= slow)
+		char *word_end = end;
+
+		while(end >= s && !sspace(*end))
+				end--;
+		char *word_start = end + 1;
+		char *p = word_start;
+
+		while(p <= word_end)
 		{
-			write(1, &str[curr], 1);
-			curr++;
+			write(1, p, 1);
+			p++;
 		}
-		// now that we wrote the word, we add a whitespace
-		// except that we should not add a whitespace for the last word
-		if (fast > 0)
-			write(1, " ", 1);
-		// and we interrupt the loop if fast is at the first char
-		// if it is not, so we move it two steps back
-		fast--;
-		fast--;
-		// now we should be at the last char of previous word
-		// so we can just repeat the process
+		while(end >= s && sspace(*end))
+			end--;
+		if(end >= s)
+			write(1, " " , 1);
 	}
+
 }
 
 int		main(int ac, char **av)
