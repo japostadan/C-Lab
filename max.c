d28614c9cc1705b9ed9eb0ac52fad016a03e2d60
japostad <japostad@student.42barcelona.com>
2025-05-22 15:17:15 +0200

    Add: practice

--- max.c
+++ max.c
@@ -1,31 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char **argv)
+/*Assignment name  : max                                                          */
+/*Expected files   : max.c                                                        */
+/*Allowed functions:                                                              */
+/*--------------------------------------------------------------------------------*/
+/*                                                                                */
+/*Write the following function:                                                   */
+/*                                                                                */
+/*int             max(int* tab, unsigned int len);                                */
+/*                                                                                */
+/*The first parameter is an array of int, the second is the number of elements in */
+/*the array.                                                                      */
+/*                                                                                */
+/*The function returns the largest number found in the array.                     */
+/*                                                                                */
+/*If the array is empty, the function returns 0.                                  */
+/*                                                                                */
+/*~/GrademeBash/42-EXAM main*                                                     */
+/*❯ cat traces/1-0_max.trace                                                      */
+/*----------------8<-------------[ START TEST                                     */
+/*        💻 TEST                                                                 */
+/*./a.out "7" "-42" "0" "-5" "-64" "-2" "-68" "-1"         🔎 YOUR OUTPUT:        */
+/*max( {-42;0;-5;-64;-2;-68;-1} , 7) = -1$                                        */
+/*        🗝 EXPECTED OUTPUT:                                                     */
+/*max( {-42;0;-5;-64;-2;-68;-1} , 7) = 0$                                         */
+/*----------------8<------------- END TEST ]                                      */
+/*                                                                                */
+int max(int *tab, unsigned int len)
 {
-    if (argc >= 2)
+    int max = 0;
+    if(len == 0)
+        return 0;
+    if(len == 1)
+        return (*tab);
+    int i = 0;
+    while(i < len)
     {
-        int i = 1;
-        int max = atoi(argv[i]);
-        int min = atoi(argv[i]);
-        // Initialize max with the first argument converted to integer
-
-        while (argv[++i]) // Loop until reaching the end of arguments
-        {
-            int num = atoi(argv[i]); // Convert each argument to integer
-            if (num > max)
-                max = num;
-            if (num < min)
-                min = num;
-        }
-
-        printf("max is %d\n", max);
-        printf("min is %d\n", min);
-        return max;
+        if(max < tab[i])
+            max = tab[i];
+        i++;
     }
-    else
-    {
-        printf("Usage: %s <num1> <num2> ... <numN>\n", argv[0]);
-        return 1;
-    }
+    return max;
 }
+/*
+#include <stdio.h>
+
+int main()
+{
+    int a[]= { -42,0,-5,-64,-2,-68,-1};
+    printf("%d\n",max(a,7));
+}
+*/
